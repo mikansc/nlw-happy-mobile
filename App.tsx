073bf4cef5cca230e69c0d8cc22cfe69301accac
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-15 13:37:16 -0300

    Initial Commit, react-native-maps implementation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -26.2770682,
+          longitude: -48.8243707,
+          longitudeDelta: 0.008,
+          latitudeDelta: 0.008,
+        }}
+      />
     </View>
   );
 }
@@ -14,8 +23,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
