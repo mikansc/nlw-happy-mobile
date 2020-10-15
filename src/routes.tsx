05c9391ac2628454ba238f979a01fb34ad28e33c
Michael Nascimento <mikanascimento@iMac-de-Michael.local>
2020-10-15 14:25:40 -0300

    Create OrphanagesDetails screen and navigation

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import { createStackNavigator } from "@react-navigation/stack";
 import OrphanagesMap from "./pages/OrphanagesMap";
+import OrphanagesDetails from "./pages/OrphanagesDetails";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -14,6 +15,11 @@ export default function Routes() {
         <Screen
           name="OrphanagesMap"
           component={OrphanagesMap}
+          options={{ headerShown: false }}
+        />
+        <Screen
+          name="OrphanagesDetails"
+          component={OrphanagesDetails}
           options={{ headerShown: false }}
         />
       </Navigator>
